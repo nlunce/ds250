069ffc313d5ba5423312ec255c8858d7fdc7d120
nlunce <naterrl15@gmail.com>
2023-04-27 16:34:18 -0600

    project 1 update

--- project1/project1.py
+++ project1/project1.py
@@ -14,53 +14,51 @@ names = pd.read_csv('names_year.csv')
 names
 
 #%%
-names.columns()
-#%%
-# Charts
-
+# Query Nathan
 nathan = names.query('name == "Nathan"')
 nathan
 
-david = names.query('name == "David"')
-david
+#%%
+# Birth year of nathan dataframe
+birthYearDf= pd.DataFrame({'year': [2002]})
+birthYearDf
+birthYearLine = alt.Chart(birthYearDf).mark_rule(color="red").encode(x = "year")
+birthYearLine
+
+#%%
+# Add label
+birthYearLabel = alt.Chart(birthYearDf).mark_text(
+    text='Birth of Nathan Lunceford (2002)',
+    align='left',
+    baseline='middle',
+    dx=10,
+    dy=250
+).encode(
+    x='year',
+    y=alt.value(0)  # adjust label position
+)
+
 #%%
 # Chart Nathan
-
-line_df = pd.DataFrame({'year': [2002]})
-line_df
-line = alt.Chart(line_df).mark_rule(color="red").encode(x = "year")
-line
-
-#%%
-
 chartNathan = (alt.Chart(nathan, title = 'Name of Nathan over the years')
   .encode(
     x = alt.X('year', axis = alt.Axis(format= 'd', title= 'Year')),
     y = alt.Y('Total', axis= alt.Axis(format= 'd', 
-                title='Children with name Nathan', ))
+                title='Children with the name Nathan', ))
     )
   .mark_line()
 )
 
 chartNathan
 
-chartNathanWithBirth = chartNathan + line
-chartNathanWithBirth
+
 
 #%%
-#Chart David
-
-chartDavid = (alt.Chart(david, title = 'Name of David over the years')
-  .encode(
-    alt.X('year'),
-    alt.Y('Total')
-    )
-  .mark_line()
-)
-chartDavid
-
-#%%
-
-chart = chartNathan + chartDavid
+# Chart Nathan + birth year DF + Label
+chart = chartNathan + birthYearLine + birthYearLabel
 chart
+
+
+
+
 
