8eba343ebe5adc0204868ea96e6a03ff635c2064
nlunce <naterrl15@gmail.com>
2023-06-07 15:45:49 -0600

    project 4 init

--- project4/project4.py
+++ project4/project4.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 # Importing
 import pandas as pd
 import altair as alt
@@ -7,11 +7,15 @@ import numpy as np
 from IPython.display import Markdown
 from IPython.display import display
 from tabulate import tabulate
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 
-#%%
+# %%
 # Load dataset
+dwell = pd.read_csv("dwellings_ml.csv")
 
+dwell.columns
 
-#%%
-# Charts
-
+# %%
+# Crosstab
